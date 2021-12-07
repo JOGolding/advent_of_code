f5fa357f4365c3d0273c32a4d2e789fa676ad9d0
JOGolding <75700366+JOGolding@users.noreply.github.com>
2021-12-07 20:08:28 +0000

    day_1_challenge_2 completed

---
+++ day_1/challenge_2.py
@@ -0,0 +1,31 @@
+from os import read
+
+
+def readFile(fileName):
+    fileObj = open(fileName, 'r')
+    numsString = fileObj.read().splitlines()
+    nums = list(map(int, numsString))
+    fileObj.close()
+    return nums
+
+numArray = readFile('day_1\input.txt')
+window = 3
+total = 0
+prevTotal = 0
+increaseCount = 0
+for x in range(len(numArray)-2):
+    print(str(numArray[x]) + ' ' + str(numArray[x+1]) + ' ' + str(numArray[x+2]))
+    total = numArray[x] + numArray[x+1] + numArray[x+2]
+    if prevTotal == 0:
+        prevTotal = total
+    elif total > prevTotal:
+        increaseCount+=1
+        prevTotal = total
+    else:
+        prevTotal = total
+
+print(increaseCount)
+
+
+
+
