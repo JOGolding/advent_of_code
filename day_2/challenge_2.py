5595754713d78be675f291bffab978bcb4aed68b
JOGolding <75700366+JOGolding@users.noreply.github.com>
2021-12-07 21:15:19 +0000

    day_2_challenge_2 refactored

--- day_2/challenge_2.py
+++ day_2/challenge_2.py
@@ -5,11 +5,12 @@ horizPos = 0
 with open(file, 'r+') as f:
     for line in f:
         lineList = line.split()
+        value = int(lineList[1])
         if lineList[0] == 'forward':
-            horizPos += int(lineList[1])
-            depth += aim*int(lineList[1])
+            horizPos += value
+            depth += aim*value
         elif lineList[0] == 'down':
-            aim += int(lineList[1])
+            aim += value
         else:
-            aim -= int(lineList[1])
+            aim -= value
 print(horizPos*depth)
